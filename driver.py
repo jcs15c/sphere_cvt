1554600c48a857aa7cf1e7e76a92d616f14718b9
Jacob Spainhour <Jacob Spainhour>
2017-04-13 22:30:04 -0400

    driver.py: updated for new functions

--- driver.py
+++ driver.py
@@ -1,20 +1,20 @@
 import spherical_utils
 import spherical_cvt
 
-N = 50
+N = 120
 M = 1000
 generators = spherical_utils.uniform_sample(N)
-
+"""
 ax = spherical_utils.init_sphere()
 
 spherical_utils.sphere_points(ax, generators)
 spherical_cvt.plot_delaunay(ax,generators)
-
+"""
 
 bins, energy = spherical_cvt.bin_points(generators, M)
 print(energy)
 
-for k in range(15):
+for k in range(5):
      spherical_cvt.cvt_step(generators, M)
      bins, energy = spherical_cvt.bin_points(generators, M)
 
