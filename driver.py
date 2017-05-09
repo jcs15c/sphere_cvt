7b5c0f369aa5110110d054e64d36c937e2efa5c6
Jacob Spainhour <jcs15c@my.fsu.edu>
2017-05-09 12:22:13 -0400

    Added functionality for plot_voronoi()

--- driver.py
+++ driver.py
@@ -1,7 +1,7 @@
 import spherical_utils
 import spherical_cvt
 
-N = 25
+N = 24
 M = 1000
 generators = spherical_utils.uniform_sample(N)
 """
@@ -14,7 +14,8 @@ spherical_cvt.plot_delaunay(ax,generators)
 bins, energy = spherical_cvt.bin_points(generators, M)
 print(energy)
 
-for k in range(5):
+
+for k in range(50):
      spherical_cvt.cvt_step(generators, M)
      bins, energy = spherical_cvt.bin_points(generators, M)
 
@@ -22,12 +23,6 @@ print(energy)
 
 ax = spherical_utils.init_sphere()
 
-#spherical_utils.sphere_points(ax, generators)
-spherical_cvt.plot_voronoi_tan(ax, generators, [0,0,-1])
-#spherical_cvt.plot_voronoi_tan(ax, generators, [0,0,-1])
-
-
-
-
+spherical_cvt.plot_voronoi(ax, generators)
 
 spherical_utils.disp_sphere(ax)
