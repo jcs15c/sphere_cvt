da530ce07ea6ace5863ad7d2b2e57b1391a96bcd
Jacob Spainhour <jcs15c@my.fsu.edu>
2017-05-09 12:23:17 -0400

    Added Voronoi and helper functions

--- spherical_cvt.py
+++ spherical_cvt.py
@@ -52,18 +52,86 @@ def bin_points(generators, M):
           
      return bins, energy
 
-#Returns indicies of delaunay triangles
+def plot_voronoi(ax, generators):
+    tri_upp, tri_low, gen_upp, gen_low \
+            = compute_delaunay(generators, True)
+        
+    pts_all, cen_all = recombine_del(gen_upp, gen_low, tri_upp, tri_low)
+    
+    ridges = calc_ridges(pts_all)
+    
+    for ridge in ridges:
+        spherical_utils.sphere_line(ax, cen_all[ridge[0]], cen_all[ridge[1]], 'r')
+    
+    return
+    
+    for tri in pts_all:
+        spherical_utils.sphere_line(ax, tri[0], tri[1], 'k')
+        spherical_utils.sphere_line(ax, tri[1], tri[2], 'k')
+        spherical_utils.sphere_line(ax, tri[2], tri[0], 'k')
+    
+    spherical_utils.sphere_points(ax, cen_all)
+    spherical_utils.disp_sphere(ax)
+    
+def calc_ridges(pts):
+    ridges = []
+    
+    for i in range(len(pts)):
+        for j in range(len(pts)):
+            if tri_adjacent(pts[i], pts[j]):
+                ridges.append([i,j])
+
+    return ridges
+    
+def tri_adjacent(tri1, tri2):
+    found = 0    
+    
+    for pt in tri1:
+        if (pt == tri2[0]).all():
+            found += 1            
+            break
+
+    for pt in tri1:
+        if (pt == tri2[1]).all():
+            found += 1            
+            break   
+
+    for pt in tri1:
+        if (pt == tri2[2]).all():
+            found += 1   
+            break
+
+    if found == 2:
+        return True
+    return False        
+    
+def plot_delaunay(ax, generators):
+    tri_upp, tri_low, gen_upp, gen_low \
+            = compute_delaunay(generators, True)
+        
+    tri_all, cen_all = recombine_del(gen_upp, gen_low, tri_upp, tri_low)
+    
+    for tri in tri_all:
+        spherical_utils.sphere_line(ax, tri[0], tri[1], 'k')
+        spherical_utils.sphere_line(ax, tri[1], tri[2], 'k')
+        spherical_utils.sphere_line(ax, tri[2], tri[0], 'k')
+    
+    spherical_utils.sphere_points(ax, cen_all)
+    spherical_utils.disp_sphere(ax)
+    
 def compute_delaunay(generators, full = 0):
     pts_upp = []
     pts_low = []
     gen_upp = []
     gen_low = []
     
+    radius = np.pi    
+    
     for i in range(len(generators)):
-        if np.arccos(np.dot([0,0,1],generators[i])) <= 3 * np.pi / 4:
+        if np.arccos(np.dot([0,0,1],generators[i])) <= radius:
             pts_upp.append(spherical_utils.project_onto_upper_plane(generators[i]))
             gen_upp.append(generators[i])
-        if np.arccos(np.dot([0,0,-1],generators[i])) <= 3 * np.pi / 4:
+        if np.arccos(np.dot([0,0,-1],generators[i])) <= radius:
             pts_low.append(spherical_utils.project_onto_lower_plane(generators[i]))
             gen_low.append(generators[i])
 
@@ -72,63 +140,87 @@ def compute_delaunay(generators, full = 0):
     
     #plot_2d_delaunay(pts_upp, tri_upp)
     
-    tri_upp = remove_ill_tri(tri_upp, pts_upp, [0,0], 3 * np.pi / 4)
-    tri_low = remove_ill_tri(tri_low, pts_low, [0,0], 3 * np.pi / 4)
+    tri_upp = remove_ill_tri(tri_upp, pts_upp, [0,0], radius)
+    tri_low = remove_ill_tri(tri_low, pts_low, [0,0], radius)
 
     #plot_2d_delaunay(pts_upp, tri_upp)
 
     if full:
         return tri_upp, tri_low, gen_upp, gen_low
     return tri_upp, tri_low
+    
+def recombine_del(gen_upp, gen_low, tri_upp, tri_low):
+    unique_tri = []    
+    unique_cen = []
 
-def plot_voronoi(ax, generators):
-    pts_upp = []
+    for tri in tri_low:
+        pt_tri = np.array([gen_low[tri[0]], gen_low[tri[1]], gen_low[tri[2]]])
+        pt_cen = spherical_utils.cir_rad_center(pt_tri[0], pt_tri[1], pt_tri[2])[1]
+        
+        unique_tri.append(pt_tri)            
+        unique_cen.append(pt_cen)            
+
+    for tri in tri_upp:
+        pt_tri = np.array([gen_upp[tri[0]], gen_upp[tri[1]], gen_upp[tri[2]]])
+        pt_cen = spherical_utils.cir_rad_center(pt_tri[0], pt_tri[1], pt_tri[2])[1]
+
+        found = False
+        
+        for i in range(len(unique_cen)):
+            if (unique_cen[i] == pt_cen).all():
+                found = True
+                break
+        if not found:
+            unique_tri.append(pt_tri)            
+            unique_cen.append(pt_cen)
+         
+    for i in range(len(unique_cen)):
+        unique_cen[i] = spherical_utils.project_to_unit_sphere(unique_cen[i])
+    
+    return unique_tri, unique_cen
+
+def plot_2d_delaunay(points, tri):
+    plt.figure()
+    points = np.asarray(points)
+    plt.triplot(points[:,0], points[:,1], tri)
+    plt.ylim([-5,5])
+    plt.xlim([-5,5])
+    plt.show()
+    
+def remove_ill_tri(tri, pts, tcen, trad):
+    i = len(tri)
+    while i > 0:
+        i += -1
+        if not is_global(pts[tri[i,0]],pts[tri[i,1]],pts[tri[i,2]], tcen, trad):
+            tri = np.delete(tri, i, axis=0)
+    return tri
+    
+def is_global(p1, p2, p3, rcen, rrad):
+    trad, tcen = spherical_utils.cir_rad_center(p1, p2, p3)
+    #tcen = spherical_utils.project_to_unit_sphere(tcen)
+    return ( (np.linalg.norm(rcen - tcen)) < rrad)
+    
+##############################################
+    #####################################
+    #####################################
+##############################################
+#Performs delaunay with only one tangent plane
+def plot_delaunay_ill(ax, generators):
+    pts = []    
     
     for i in range(len(generators)):
-        if np.arccos(np.dot([0,0,1],generators[i])) <= 4 * np.pi / 5:
-            pts_upp.append(spherical_utils.project_onto_upper_plane(generators[i]))
-       # if np.arccos(np.dot([0,0,-1],generators[i])) <= 3 * np.pi / 4:
-    
-    vor_upp = Voronoi(pts_upp)
-    plot_internal_voronoi(vor_upp)
-    
-    rv_upp = vor_upp.ridge_vertices
-    rv_upp = int_ridge(rv_upp)
-
-    v_upp_plane = vor_upp.vertices
-    v_upp_sphere = np.zeros([v_upp_plane.shape[0],3])
+        pts.append(spherical_utils.project_onto_upper_plane(generators[i]))
         
-    for i in range(v_upp_plane.shape[0]):
-        v_upp_sphere[i] = spherical_utils.project_onto_upper_sphere(v_upp_plane[i])
-       
-    for ind in rv_upp:
-        p1 = v_upp_sphere[ind[0]]
-        p2 = v_upp_sphere[ind[1]]
-        spherical_utils.sphere_line(ax,p1,p2)
+    tri = Delaunay(pts)
+    
+    inds = tri.simplices.copy()
+    
+    for ind in inds:
+        spherical_utils.sphere_line(ax, generators[ind[0]], generators[ind[1]])
+        spherical_utils.sphere_line(ax, generators[ind[1]], generators[ind[2]])
+        spherical_utils.sphere_line(ax, generators[ind[2]], generators[ind[0]])
         
-    pts_low = []
-    
-    for i in range(len(generators)):
-        if np.arccos(np.dot([0,0,-1],generators[i])) <= 4 * np.pi / 5:
-            pts_low.append(spherical_utils.project_onto_lower_plane(generators[i]))
-       # if np.arccos(np.dot([0,0,-1],generators[i])) <= 3 * np.pi / 4:
-    
-    vor_low = Voronoi(pts_low)
-    plot_internal_voronoi(vor_low)
-    
-    rv_low = vor_low.ridge_vertices
-    rv_low = int_ridge(rv_low)
-
-    v_low_plane = vor_low.vertices
-    v_low_sphere = np.zeros([v_low_plane.shape[0],3])
-        
-    for i in range(v_low_plane.shape[0]):
-        v_low_sphere[i] = spherical_utils.project_onto_lower_sphere(v_low_plane[i])
-       
-    for ind in rv_low:
-        p1 = v_low_sphere[ind[0]]
-        p2 = v_low_sphere[ind[1]]
-        spherical_utils.sphere_line(ax,p1,p2)
+    spherical_utils.disp_sphere(ax)
 
 def plot_voronoi_tan(ax, generators, tangent):
     pts = []
@@ -154,7 +246,7 @@ def plot_voronoi_tan(ax, generators, tangent):
         p1 = vertices_sphere[ind[0]]
         p2 = vertices_sphere[ind[1]]
         spherical_utils.sphere_line(ax,p1,p2)
-      
+
 def plot_internal_voronoi(vor):
     plt.figure()
     ridge_vertices = vor.ridge_vertices
@@ -184,63 +276,5 @@ def int_ridge(ridge_vertices):
            ridge_vertices[i][1] == -1:
             ridge_vertices = np.delete(ridge_vertices, i, axis=0)
     return ridge_vertices
-    
-    
-def plot_delaunay(ax, generators):
-    tri_upp, tri_low, gen_upp, gen_low \
-            = compute_delaunay(generators, True)
-        
-    for tri in tri_low:
-        spherical_utils.sphere_line(ax, gen_low[tri[0]], gen_low[tri[1]], 'b')
-        spherical_utils.sphere_line(ax, gen_low[tri[1]], gen_low[tri[2]], 'b')
-        spherical_utils.sphere_line(ax, gen_low[tri[2]], gen_low[tri[0]], 'b')
-    
-    for tri in tri_upp:
-        spherical_utils.sphere_line(ax, gen_upp[tri[0]], gen_upp[tri[1]], 'r')
-        spherical_utils.sphere_line(ax, gen_upp[tri[1]], gen_upp[tri[2]], 'r')
-        spherical_utils.sphere_line(ax, gen_upp[tri[2]], gen_upp[tri[0]], 'r')
-    
-    spherical_utils.disp_sphere(ax)
-
-def plot_2d_delaunay(points, tri):
-    plt.figure()
-    points = np.asarray(points)
-    plt.triplot(points[:,0], points[:,1], tri)
-    plt.ylim([-5,5])
-    plt.xlim([-5,5])
-    plt.show()
-    
-def remove_ill_tri(tri, pts, tcen, trad):
-    i = len(tri)
-    while i > 0:
-        i += -1
-        if not is_global(pts[tri[i,0]],pts[tri[i,1]],pts[tri[i,2]], tcen, trad):
-            tri = np.delete(tri, i, axis=0)
-    return tri
-    
-def is_global(p1, p2, p3, rcen, rrad):
-    trad, tcen = spherical_utils.cir_rad_center(p1, p2, p3)
-    #tcen = spherical_utils.project_to_unit_sphere(tcen)
-    return ( (np.linalg.norm(rcen - tcen)) < rrad)
-
-#Performs delaunay with only one tangent plane
-def plot_delaunay_ill(ax, generators):
-    pts = []    
-    
-    for i in range(len(generators)):
-        pts.append(spherical_utils.project_onto_upper_plane(generators[i]))
-        
-    tri = Delaunay(pts)
-    
-    inds = tri.simplices.copy()
-    
-    for ind in inds:
-        spherical_utils.sphere_line(ax, generators[ind[0]], generators[ind[1]])
-        spherical_utils.sphere_line(ax, generators[ind[1]], generators[ind[2]])
-        spherical_utils.sphere_line(ax, generators[ind[2]], generators[ind[0]])
-        
-    spherical_utils.disp_sphere(ax)
-
-
 
 
