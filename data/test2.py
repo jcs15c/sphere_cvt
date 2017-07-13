fe0ab07de238bb54dd05c37b27972033905d6863
jcs15c <JacobSpainhour782@gmail.com>
2017-07-13 14:42:00 -0400

    Delete test2.py

--- data/test2.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-xs = np.genfromtxt("xs.csv", delimiter=",")
-ys = np.genfromtxt("ys.csv", delimiter=",")
-
-plt.plot(xs,ys,'bo')
-plt.axis([-180, 180, -90, 90])
-plt.show()
